b8b1c12a9109331e15d8c5ab5e9ba2006cb5e1b8
rodrigo simões <rodrigosimoes@ua.pt>
2025-12-21 17:29:34 +0000

    eliminar comentarios

--- sql/Triggers/TR_AbaterStockVenda.sql
+++ sql/Triggers/TR_AbaterStockVenda.sql
@@ -10,7 +10,6 @@ AS
 BEGIN
     SET NOCOUNT ON;
     
-    -- Atualiza o stock cruzando informações de Item -> Venda -> Loja -> Armazém
     UPDATE S
     SET S.Quantidade = S.Quantidade - I.Quantidade
     FROM dbo.Stock S
@@ -19,7 +18,5 @@ BEGIN
     INNER JOIN dbo.Loja L ON V.Loja_Id = L.Id
     WHERE S.Armazem_Id = L.Armazem_Id;
 
-    -- Nota: Se o stock ficar negativo, o trigger 'GarantirStockPositivo' 
-    -- (se ativo) irá reverter esta transação automaticamente.
 END
 GO
